40446da4d314c702d85a9d7d82ed6263376c1d48
ARYA GILANG <91578406+Ag-Gilang@users.noreply.github.com>
2021-10-29 07:33:55 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -3,7 +3,11 @@ let handler = async (m, { text }) => {
   user.afk = + new Date
   user.afkReason = text
   m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+*「 AFK MODE 」*
+
+${conn.getName(m.sender)} Sekarang Sedang AFK
+
+Alasan _${text ? ': ' + text : ''}_
 `)
 }
 handler.help = ['afk [alasan]']
