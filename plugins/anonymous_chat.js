4e4a523f3f3fe12ace4a67635e2387816d28708b
ARYA GILANG <91578406+Ag-Gilang@users.noreply.github.com>
2021-10-30 08:59:31 +0700

    Update anonymous_chat.js

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -49,7 +49,7 @@ async function handler(m, { command }) {
 handler.help = ['start', 'leave', 'next']
 handler.tags = 'anonymous'
 
-handler.command = ['start', 'leave', 'next']
+handler.command = /^(start|leave|next)$/i
 handler.private = true
 
 module.exports = handler
